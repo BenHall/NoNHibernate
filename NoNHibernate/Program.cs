de68785c4186cac5879c99ad405752bcf1bd86da
Ben Hall <Ben@Benhall.me.uk>
2011-01-06 14:12:51 +0000

    Formatting of Program

--- NoNHibernate/Program.cs
+++ NoNHibernate/Program.cs
@@ -5,32 +5,34 @@ using AutoMapper;
 
 namespace NoNHibernate
 {
-	class Program
-	{
-		static void Main(string[] args) {
-			Mapper.Reset();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mapper.Reset();
             Mapper.Initialize(x => x.CreateMap<IDataReader, Release>());
 
             IEnumerable<Release> releases = ExampleQuery();
 
-			foreach (var release in releases) {
-				Console.WriteLine(release.productId);
-				Console.WriteLine(release.productName);
-			}
+            foreach (var release in releases)
+            {
+                Console.WriteLine(release.productId);
+                Console.WriteLine(release.productName);
+            }
 
-			Console.ReadLine();
-		}
+            Console.ReadLine();
+        }
 
         private static IEnumerable<Release> ExampleQuery()
         {
             Query<Release> query = new Query<Release>()
-									.Where(x => x.productName == "Test Product")
-									.SetMaxResults(1);
+                                    .Where(x => x.productName == "Test Product")
+                                    .SetMaxResults(1);
 
             var executor = new DatabaseQueryExecutor();
             IEnumerable<Release> releases = executor.Execute(query);
 
-			return releases;
-		}
-	}
+            return releases;
+        }
+    }
 }
